6d70280e15b4da39e899eb216b3ed64e923fb934
Constantine Fedenko <cfedenko@gmail.com>
2013-11-23 13:13:55 +0200

    admin pages like frontend

--- wdtest/apps/imglist/models.py
+++ wdtest/apps/imglist/models.py
@@ -4,18 +4,17 @@ from django.conf import settings
 UPLOAD_TO = getattr(settings, "UPLOAD_IMAGES_DIR", "img")
 
 
-class Image(models.Model):
-    title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to=UPLOAD_TO)
+class ImageList(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    creator_ip = models.IPAddressField(blank=True, null=True)
 
     def __unicode__(self):
         return self.title
 
-
-class ImageList(models.Model):
-    title = models.CharField(max_length=255, unique=True)
-    creator_ip = models.IPAddressField(blank=True, null=True)
-    images= models.ManyToManyField(Image, related_name="lists")
+class Image(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to=UPLOAD_TO)
+    lists= models.ManyToManyField(ImageList, related_name="images")
 
     def __unicode__(self):
         return self.title
