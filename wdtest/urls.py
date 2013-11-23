a33af3e93ef1ca8722255adefee470d8e2c16613
Constantine Fedenko <cfedenko@gmail.com>
2013-11-23 12:46:39 +0200

    serve static content in debug mode

--- wdtest/urls.py
+++ wdtest/urls.py
@@ -1,5 +1,5 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
 
@@ -7,3 +7,10 @@ urlpatterns = patterns('',
     url('', include('imglist.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += patterns(
+        'django.contrib.staticfiles.views',
+        url(r'^static/(?P<path>.*)$', 'serve'),
+    ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
