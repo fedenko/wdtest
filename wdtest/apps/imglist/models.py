2b40c68e166e1d93dd793d9bef1c35c289f58fd5
Constantine Fedenko <cfedenko@gmail.com>
2013-11-23 13:08:29 +0200

    basic models

--- wdtest/apps/imglist/models.py
+++ wdtest/apps/imglist/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+UPLOAD_TO = getattr(settings, "UPLOAD_IMAGES_DIR", "img")
+
+
+class Image(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to=UPLOAD_TO)
+
+    def __unicode__(self):
+        return self.title
+
+
+class ImageList(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    creator_ip = models.IPAddressField(blank=True, null=True)
+    images= models.ManyToManyField(Image, related_name="lists")
+
+    def __unicode__(self):
+        return self.title
